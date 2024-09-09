78134b10d816c96946722d58f36b8ac8dfea3d4c
Benedict Zendel <65031715+jhin-mista@users.noreply.github.com>
2024-09-09 23:20:49 +0200

    Add git tag retriever | #11

---
+++ test/ReleaseCreator.Git.Tests/PowerShellExecutorTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+
+namespace ReleaseCreator.Git.Tests
+{
+    [TestFixture]
+    class PowerShellExecutorTest
+    {
+        private PowerShellExecutor _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new();
+        }
+
+        [Test]
+        public void Execute_ShouldExecuteCommandAndReturnExpectedResult()
+        {
+            // arrange
+            var expectedResult = "test";
+            var script = $"echo {expectedResult}";
+
+            // act
+            var result = _sut.Execute(script);
+
+            // assert
+            result.Should().ContainSingle();
+
+            result.Single().BaseObject.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Test]
+        public void Execute_WhenScriptErroneous_ShouldThrow()
+        {
+            // arrange
+            var script = "not a known command";
+
+            // act
+            var invocation = _sut.Invoking(x => x.Execute(script));
+
+            // assert
+            invocation.Should().Throw<AggregateException>().WithMessage($"Error(s) executing script '{script}'*");
+        }
+    }
+}
---
+++ test/ReleaseCreator.Git.Tests/TagRetrieverTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Moq;
+using System.Management.Automation;
+
+namespace ReleaseCreator.Git.Tests
+{
+    [TestFixture]
+    public class TagRetrieverTest
+    {
+        private TagRetriever _sut;
+        private Mock<IPowerShellExecutor> _powerShellExecutorMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _powerShellExecutorMock = new(MockBehavior.Strict);
+            _sut = new(_powerShellExecutorMock.Object);
+        }
+
+        [Test]
+        public void GetLatestTag_WhenTagExists_ShouldReturnTagName()
+        {
+            // arrange
+            var path = "path/to/somewhere";
+            var expectedTagName = "tag";
+            var powerShellResult = new PSObject(expectedTagName);
+
+            _powerShellExecutorMock.Setup(x => x.Execute(It.IsAny<string>())).Returns([powerShellResult]);
+
+            // act
+            var actualTagName = _sut.GetLatestTag(path);
+
+            // assert
+            var expectedScript = $@"Set-Location {path}
+git tag --sort=-v:refname | Select-Object -First 1";
+
+            actualTagName.Should().Be(expectedTagName);
+
+            _powerShellExecutorMock.Verify(x => x.Execute(expectedScript), Times.Once);
+            _powerShellExecutorMock.VerifyNoOtherCalls();
+        }
+    }
+}
