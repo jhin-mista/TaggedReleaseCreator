0c660605fdbb0b7b13f79dad9c18b01ad692c85f
Benedict Zendel <65031715+jhin-mista@users.noreply.github.com>
2024-09-28 19:44:36 +0200

    Add DI extension

---
+++ src/ReleaseCreator.Git/Extensions/GitServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ReleaseCreator.Git.ShellExecution;
+using ReleaseCreator.Git.Tag;
+
+namespace ReleaseCreator.Git.Extensions
+{
+    /// <summary>
+    /// Extensions methods for adding git services to the DI container.
+    /// </summary>
+    public static class GitServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds services required for using git.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        public static IServiceCollection AddVersionIncrementorServicesSingleton(this IServiceCollection services)
+        {
+            services.AddSingleton<TagRetriever>();
+            services.AddSingleton<IPowerShellExecutor, PowerShellExecutor>();
+
+            return services;
+        }
+    }
+}
