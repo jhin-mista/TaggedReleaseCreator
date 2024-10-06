2aab52f685c210e045c37de7a797b5e21ecef5c8
jhin-mista <65031715+jhin-mista@users.noreply.github.com>
2024-10-06 08:58:44 +0200

    Add /github/workspace as safe gut directory | #3

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 mv /app /github/workspace/ReleaseCreator
 # Make binaries executable
 chmod -R +x /github/workspace/ReleaseCreator
+git config --global --add safe.directory /github/workspace
 dotnet /github/workspace/ReleaseCreator/ReleaseCreator.CommandLine.dll $@
