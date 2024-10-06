4fe80a97f480987b3c4c27f4303c3954066be6aa
Benedict Zendel <65031715+jhin-mista@users.noreply.github.com>
2024-10-06 12:46:23 +0200

    Remove empty string args in entrypoint.sh | #3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 mv /app /github/workspace/ReleaseCreator
+
 # Make binaries executable
 chmod -R +x /github/workspace/ReleaseCreator
+
 git config --global --add safe.directory /github/workspace
+
+# Only take non empty arguments
+for arg in "$@"
+do
+    if [ -n "$arg" ]; then
+        arguments="${arguments} $arg";
+    fi
+done
+set -- $arguments
+
 dotnet /github/workspace/ReleaseCreator/ReleaseCreator.CommandLine.dll $@
