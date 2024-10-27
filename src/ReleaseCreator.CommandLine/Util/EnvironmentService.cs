a9c910d90537b6dc5d037350f49d8786adb8425a
Benedict Zendel <65031715+jhin-mista@users.noreply.github.com>
2024-10-27 21:37:23 +0100

    Use block body methods | #16

--- src/ReleaseCreator.CommandLine/Util/EnvironmentService.cs
+++ src/ReleaseCreator.CommandLine/Util/EnvironmentService.cs
@@ -3,6 +3,9 @@
 internal class EnvironmentService : IEnvironmentService
 {
     /// <exception cref="Exception"></exception>
-    public string GetRequiredEnvironmentVariable(string name) => Environment.GetEnvironmentVariable(name)
+    public string GetRequiredEnvironmentVariable(string name)
+    {
+        return Environment.GetEnvironmentVariable(name)
         ?? throw new Exception($"Environment variable '{name}' is not set but required.");
+    }
 }
--- src/ReleaseCreator.CommandLine/Util/FileService.cs
+++ src/ReleaseCreator.CommandLine/Util/FileService.cs
@@ -2,6 +2,9 @@
 {
     internal class FileService : IFileService
     {
-        public void AppendLine(string filePath, string content) => File.AppendAllLines(filePath, [content]);
+        public void AppendLine(string filePath, string content)
+        {
+            File.AppendAllLines(filePath, [content]);
+        }
     }
 }
