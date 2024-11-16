ccbf5437925a6e2cab2fe62ad5a5f38245bd87e1
Benedict Zendel <65031715+jhin-mista@users.noreply.github.com>
2024-11-16 22:05:49 +0100

    Add git tag retriever | #11

---
+++ src/ReleaseCreator.Git/IPowerShellExecutor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+namespace ReleaseCreator.Git
+{
+    internal interface IPowerShellExecutor
+    {
+        public Collection<PSObject> Execute(string script);
+    }
+}
