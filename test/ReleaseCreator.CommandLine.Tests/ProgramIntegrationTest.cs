f5486574399b2d747ae551e0aa919d39e112029c
Benedict Zendel <65031715+jhin-mista@users.noreply.github.com>
2024-11-16 22:06:38 +0100

    Add integration test | #14

---
+++ test/ReleaseCreator.CommandLine.Tests/ProgramIntegrationTest.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Moq;
+using Octokit;
+
+namespace ReleaseCreator.CommandLine.Tests
+{
+    [TestFixture]
+    public class ProgramIntegrationTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            var workingDirectory = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "TestRepository");
+            Environment.CurrentDirectory = workingDirectory;
+            Directory.Move(".igit", ".git");
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Directory.Move(".git", ".igit");
+        }
+
+        [Test]
+        public async Task Main_ShouldCreateNextReleaseAndCallClient()
+        {
+            // arrange
+            long repositoryId = -123456789;
+            Environment.SetEnvironmentVariable("GITHUB_REPOSITORY_ID", repositoryId.ToString());
+
+            var clientMock = new Mock<IReleasesClient>(MockBehavior.Strict);
+            var createdRelease = new Release();
+            NewRelease? calculatedNewRelease = null;
+            clientMock.Setup(x => x.Create(It.IsAny<long>(), It.IsAny<NewRelease>()))
+                .Callback<long, NewRelease>((_, x) => calculatedNewRelease = x)
+                .ReturnsAsync(createdRelease);
+
+            Program.ReleasesClientFactory = _ => clientMock.Object;
+
+            string[] arguments =
+                [
+                    "--branch", "main",
+                    "--type", "major",
+                    "--token", "token",
+                ];
+
+            // act
+            var result = await Program.Main(arguments);
+
+            // assert
+            result.Should().Be(0);
+
+            clientMock.Verify(x => x.Create(repositoryId, It.IsAny<NewRelease>()), Times.Once);
+            clientMock.VerifyNoOtherCalls();
+
+            calculatedNewRelease.Should().NotBeNull();
+            calculatedNewRelease!.TagName.Should().Be("v2.0.0");
+        }
+    }
+}
